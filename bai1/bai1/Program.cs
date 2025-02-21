f2313206df4d13955e8bd02d0b8b9d42c2440f23
NguyenVanKhanh1308 <khanhvn1308@gmail.com>
2025-02-21 23:04:21 +0700

    v1

---
+++ bai1/bai1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int i221, n221;
+        long S221;
+        S221 = 0;
+        i221 = 1;
+
+        Console.Write("\nNhap n: ");
+        n221 = Convert.ToInt32(Console.ReadLine());
+
+        while (i221 <= n221)
+        {
+            S221 = S221 + i221;
+            i221++;
+        }
+
+        Console.WriteLine("\nTong 1 + 2 + ... + {0} la {1}", n221, S221);
+        Console.ReadKey();
+    }
+}
